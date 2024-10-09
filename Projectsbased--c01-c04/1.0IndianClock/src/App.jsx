e6b802c49b3c1ef4d7a9910fb4eb8dc9da87ef63
ChandraShekhar <104549787+cs350892@users.noreply.github.com>
2024-10-09 09:29:55 +0530

    Add files via upload
    what u have learnt from chapter 1 to chapter 4 Now Its time to showcase ur pratical Knoledge---HatsOffChandra

---
+++ Projectsbased--c01-c04/1.0IndianClock/src/App.jsx
@@ -0,0 +1,12 @@
+import Title from "./components/Title";
+import DisplayItem from "./components/DisplayItem";
+
+function App() {
+  return (
+    <>
+      <Title></Title>
+      <DisplayItem></DisplayItem>
+    </>
+  );
+}
+export default App;
